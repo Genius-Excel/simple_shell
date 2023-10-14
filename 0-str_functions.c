335530bc26dd510cab70aaf236ae8312412a90ea
Genius-Excel <geniusexcel9@gmail.com>
2023-10-14 16:40:35 -0700

    This file contains the string helper functions.

---
+++ 0-str_functions.c
@@ -0,0 +1,12 @@
+#include "shell.h"
+
+
+/**
+* _putchar - this function prints a character to the standard output.
+* @c: character to be printed.
+* Return: the printed character to stdout.
+*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
