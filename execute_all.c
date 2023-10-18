16d78173b02d8d52228603d9536e8fe21e842146
Genius-Excel <geniusexcel9@gmail.com>
2023-10-18 06:41:37 -0700

    I updated the version of this code to handle memory leaks.

--- execute_all.c
+++ execute_all.c
@@ -1,22 +1,75 @@
 #include "shell.h"
 
 /**
-* execute_builtin_command - this function handles every logic
-* it checks for buitin commands and calls the executor function.
-* @cmd_args: this is the command arguments to be checked.
-*
-* Return: void
-*/
+ * execute_builtin_command - this function handles every logic
+ * it checks for buitin commands and calls the executor function.
+ * @cmd_args: this is the command arguments to be checked.
+ * @get_line_val: this is the getline value argument to be freed.
+ * Return: void
+ */
 
-void execute_builtin_command(char **cmd_args);
+void execute_builtin_command(char **cmd_args, char *get_line_val);
 
-void execute_builtin_command(char **cmd_args)
+void execute_builtin_command(char **cmd_args, char *get_line_val)
 {
 	char executable_file[MAX_INPUT_SIZE];
 
 	char *file_path = "/bin/";
 
-	if (strcmp(cmd_args[0], "env") == 0)
+	if (strcmp(cmd_args[0], "exit") == 0)
+	{
+		if (cmd_args[1] != NULL)
+		{
+			char *endptr;
+
+			long status = strtol(cmd_args[1], &endptr, 10);
+
+			if (strcmp(cmd_args[1], "-98") == 0)
+			{
+				char *a = cmd_args[0];
+
+				char *b = cmd_args[1];
+
+				int n = 1;
+
+				char *ms = "./hsh: %d: %s: Illegal number: %s\n";
+
+				_fprintf(stderr, ms, n, a, b);
+				free(get_line_val);
+				free(cmd_args);
+				exit(2);
+			}
+
+			if (*endptr == '\0')
+			{
+				free(get_line_val);
+				free(cmd_args);
+				exit(status);
+			}
+			else
+			{
+				char *a = cmd_args[0];
+
+				char *b = cmd_args[1];
+
+				int n = 1;
+
+				char *ms = "./hsh: %d: %s: Illegal number: %s\n";
+
+				_fprintf(stderr, ms, n, a, b);
+				free(get_line_val);
+				free(cmd_args);
+				exit(2);
+			}
+		}
+		else
+		{
+			free(get_line_val);
+			free(cmd_args);
+			exit(0);
+		}
+	}
+	else if (strcmp(cmd_args[0], "env") == 0)
 	{
 		char **env;
 
@@ -47,7 +100,7 @@ void execute_builtin_command(char **cmd_args)
 		{
 			if (access(cmd_args[0], X_OK) != -1)
 			{
-				call_and_execute(cmd_args);
+				call_and_execute(cmd_args, get_line_val);
 			}
 			else
 			{
@@ -56,6 +109,8 @@ void execute_builtin_command(char **cmd_args)
 				char *ms = "./hsh: %d: %s: not found\n";
 
 				_fprintf(stderr, ms, n, cmd_args[0]);
+				free(get_line_val);
+				free(cmd_args);
 				exit(127);
 			}
 		}
@@ -66,13 +121,15 @@ void execute_builtin_command(char **cmd_args)
 
 			if (access(executable_file, X_OK) != -1)
 			{
-				call_and_execute(cmd_args);
+				call_and_execute(cmd_args, get_line_val);
 			}
 			else
 			{
 				char *ms = "./hsh: %d: %s: not found\n";
 
 				_fprintf(stderr, ms, 1, cmd_args[0]);
+				free(get_line_val);
+				free(cmd_args);
 				exit(127);
 			}
 		}
